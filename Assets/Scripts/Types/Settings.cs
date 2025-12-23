c395f0d4a4de32d17067153d912ec42eb99248f8
User <grishaev3@gmail.com>
2025-12-23 18:32:37 +0300

    Изменения для AMD

--- Assets/Scripts/Types/Settings.cs
+++ Assets/Scripts/Types/Settings.cs
@@ -2,18 +2,18 @@
 
 class Settings
 {
-    public int TargetFPS => 100;
+    public int TargetFPS => 24;
     public int SyncCount => 0;
 
     public float SleepThreshold => 0.005f;
     public int DefaultSolverIterations => 20;
     public int DefaultSolverVelocityIterations => 8;
 
-    public int HourStart = 4;
-    public int HourEnd = 10;
+    public int HourStart = 0;
+    public int HourEnd = 23;
 
     public TimeSpan CameraModelDuration => TimeSpan.FromSeconds(20);
-    public TimeSpan SunPeriodDuration => TimeSpan.FromSeconds(3);
+    public TimeSpan SunPeriodDuration => TimeSpan.FromSeconds(5);
 
     public string VolumeName => "Глобальный обём";
 }
